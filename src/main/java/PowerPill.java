46bce4c34379005ce4b58f989d2681aab5491d77
DSHEP2024 <djshepherd23@students.desu.edu>
2025-04-01 19:13:28 -0400

    finished

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -7,32 +7,80 @@
  * @author (You)
  * @version (0.1)
  */
-public class PowerPill
-{
-    // TODO - replace this line with instruction from step 1
+public class PowerPill {
 
-    // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    // Static integer field for the default power value
+    public static final int DEFAULT_POWER = 10;
 
-    // constructors
+    // Instance variables for power and name
+    private int power;
+    private String name;
 
-    // TODO - replace this line with instruction from step 4
+    /**
+     * Initializes this power pill to a default power value
+     * and sets the name of the pill to name.
+     * @param name the name of this power pill.
+     */
+    public PowerPill(String name) {
+        this.name = name;
+        this.power = DEFAULT_POWER;  // Default power value
+    }
 
-    // TODO - replace this line with instruction from step 5
+    /**
+     * Initializes this power pill to the value of power
+     * and sets the name of the pill to name.
+     * @param name the name of this power pill
+     * @param power the power level of this power pill.
+     */
+    public PowerPill(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
 
+    // Getter for the name
+    /**
+     * Gets the name of the power pill.
+     * @return the name of this power pill.
+     */
+    public String getName() {
+        return name;
+    }
 
-    // accessor methods
+    // Getter for the power
+    /**
+     * Gets the power level of the power pill.
+     * @return the power level of this power pill.
+     */
+    public int getPower() {
+        return power;
+    }
 
-    // TODO - replace this line with instruction from step 6
+    // Setter for the name
+    /**
+     * Sets the name of this power pill.
+     * @param name the new name of this power pill.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-
-    // mutator methods
-
-    // TODO - replace this line with instruction from step 7
+    // Setter for the power
+    /**
+     * Sets the power level of this power pill.
+     * @param power the new power level of this power pill.
+     */
+    public void setPower(int power) {
+        this.power = power;
+    }
 
     // toString method
-
-    // TODO - replace this line with instruction from step 8
-
+    /**
+     * Returns a string representation of the power pill in the format:
+     * PowerPill <name> = <power>
+     * @return a string representation of the power pill.
+     */
+    @Override
+    public String toString() {
+        return "PowerPill " + name + " = " + power;
+    }
 }
